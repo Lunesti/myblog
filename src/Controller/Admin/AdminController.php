f59646e177f30294506d1c5c8c105a5d9e4f3a94
Arben.peposi <arben.peposi@outlook.fr>
2021-05-19 08:54:30 +0200

    creations des vues + controller create post et les formType

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use  App\Entity\Post;
+use App\Form\PostType;
+use App\Entity\Comment;
+use App\Form\SendCommentType;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+class AdminController extends AbstractController
+{
+    public function __construct(PostRepository $repository, EntityManagerInterface $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    /**
+     * create
+     * @Route("/admin/post/create", name="admin.post.create")
+     * @param  mixed $request
+     * @return Response
+     */
+    public function create(Request $request): Response
+    {
+        $posts = new Post();
+
+        $form = $this->createForm(PostType::class, $posts);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($posts);
+            $this->em->flush();
+            $this->addFlash('success', 'Article ajouté avec succès');
+
+            return $this->redirectToRoute('posts');
+        }
+
+        return $this->render('admin/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
